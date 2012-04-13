e5ddaae67615a403f1b172a64c79eb8d54f49f86
Colton Fent <colton@jrfent.com>
2012-04-13 10:30:53 -0700

    twitter bootstrap && devise

--- Gemfile
+++ Gemfile
@@ -9,23 +9,17 @@ gem 'sqlite3'
 gem 'twitter-bootstrap-rails'
 gem 'devise'
 gem 'inherited_resources'
+gem 'rspec-rails'
+gem 'capybara'
+gem 'nyan-cat-formatter'
+
 
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
-
-  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platform => :ruby
-
   gem 'uglifier', '>= 1.0.3'
 end
 
 gem 'jquery-rails'
-
-group :test do
-  gem 'rspec-rails'
-  gem 'capybara'
-  gem 'nyan-cat-formatter'
-end
--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
@@ -49,6 +50,9 @@ module G5stars
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
+    
+    #devise
+    config.assets.initialize_on_precompile = false
 
     # Enable the asset pipeline
     config.assets.enabled = true
