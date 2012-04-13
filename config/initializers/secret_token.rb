3031700c27fe4278a62efb284507079fd1ef7807
Colton Fent <colton@jrfent.com>
2012-04-13 10:15:20 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+G5stars::Application.config.secret_token = '473076e26f6d595afff42ddd2eb631fd2dcd097b0ee3fd4676d104efd29a8a20ca5d54d2f0902b8e0490301b987b0219f71eb6065edc2db43cd0d4d0e322b92d'
