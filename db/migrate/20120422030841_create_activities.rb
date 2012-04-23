6b032b8a8188e9cd1eafc269a9a9fdfaaff9128f
Michael Mitchell <michaelmitchell@hush.com>
2012-04-22 20:12:02 -0700

    change migration to migrate activity data from star to activity

--- db/migrate/20120422030841_create_activities.rb
+++ db/migrate/20120422030841_create_activities.rb
@@ -1,10 +1,14 @@
 class CreateActivities < ActiveRecord::Migration
-  def change
+  def up
     create_table :activities do |t|
       t.string :name
       t.text :description
 
       t.timestamps
     end
+
+  end
+  def down
+    drop_table :activities
   end
 end
--- db/migrate/20120422031249_add_activity_id_volunteer_id_to_stars.rb
+++ db/migrate/20120422031249_add_activity_id_volunteer_id_to_stars.rb
@@ -1,5 +1,17 @@
 class AddActivityIdVolunteerIdToStars < ActiveRecord::Migration
-  def change
+  def up
     add_column :stars, :activity_id, :integer
+    Star.reset_column_information 
+    stars = Star.all
+    stars.each do |star|
+      activity = Activity.find_or_create_by_name_and_description(star.name, star.description)
+      star.activity = activity
+      star.save
+    end
   end
+
+  def down
+    remove_column :stars, :activity_id
+  end
+
 end
