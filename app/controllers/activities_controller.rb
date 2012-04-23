cf7ea22fde7cdc1d66dd95b32f8a3b76440fc9ec
Michael Mitchell <michaelmitchell@hush.com>
2012-04-22 22:29:14 -0700

    add autocomplete activity field for new star

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,7 @@
 class ActivitiesController < ApplicationController
   def index
+    @activities = Activity.order(:name).where("name like ?", "#{params[:term]}")
+    render json: @activities.map(&:name)
   end
 
   def show
--- app/models/star.rb
+++ app/models/star.rb
@@ -2,8 +2,15 @@ class Star < ActiveRecord::Base
   belongs_to :volunteer
   belongs_to :activity
 
-  attr_accessible :activity, :board_member
+  attr_accessible :activity, :board_member, :activity_name
 
   scope :ordered, order("board_member DESC")
 
+  def activity_name
+    activity.try(:name)
+  end
+
+  def activity_name=(name)
+    self.activity = Activity.find_or_create_by_name(name) if name.present?
+  end
 end
