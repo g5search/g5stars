1073b3139c4762e1ae0e3d907528d257c4eec65c
Michael Mitchell <michael.mitchell@g5searchmarketing.com>
2012-05-04 18:48:23 -0700

    add everything to the group by clause

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -12,7 +12,7 @@ class Volunteer < ActiveRecord::Base
 
   scope :by_stars, select("volunteers.*, count(stars.id) as stars_count").
                    joins("LEFT JOIN stars ON volunteers.id = stars.volunteer_id").
-                   group('volunteers.first_name, volunteers.last_name, volunteers.id, volunteers.photo_url').
+                   group('volunteers.first_name, volunteers.last_name, volunteers.id, volunteers.photo_url, volunteers.created_at, volunteers.updated_at').
                    order("stars_count DESC, first_name, last_name")
 
   BASE_URL = 'http://www.g5platform.com.g5demo.com/g5_team'
